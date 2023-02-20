d6b472a0b74a64ea514f54eee1b1097fd1832485
akrem <akrem@NIKO>
2023-02-20 22:10:35 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace SystemProgramming_HW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.EndBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.RefReshBtn = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(315, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(326, 513);
+            this.panel2.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(3, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(320, 36);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Process Name";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(3, 49);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(320, 464);
+            this.listBox2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(373, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(378, 516);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button5);
+            this.panel4.Controls.Add(this.listBox3);
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Location = new System.Drawing.Point(647, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(308, 512);
+            this.panel4.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(6, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(302, 36);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Priority";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(6, 48);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(302, 464);
+            this.listBox3.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(373, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(378, 516);
+            this.panel5.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.RefReshBtn);
+            this.panel6.Controls.Add(this.textBox1);
+            this.panel6.Controls.Add(this.EndBtn);
+            this.panel6.Controls.Add(this.StartBtn);
+            this.panel6.Location = new System.Drawing.Point(1, 522);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(954, 64);
+            this.panel6.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // EndBtn
+            // 
+            this.EndBtn.Location = new System.Drawing.Point(442, 8);
+            this.EndBtn.Name = "EndBtn";
+            this.EndBtn.Size = new System.Drawing.Size(119, 39);
+            this.EndBtn.TabIndex = 2;
+            this.EndBtn.Text = "End Process";
+            this.EndBtn.UseVisualStyleBackColor = true;
+            this.EndBtn.Click += new System.EventHandler(this.EndBtn_Click);
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(300, 8);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(119, 39);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "Start Process";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(3, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 464);
+            this.listBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 517);
+            this.panel1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(3, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(302, 36);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Process Id";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // RefReshBtn
+            // 
+            this.RefReshBtn.Location = new System.Drawing.Point(582, 8);
+            this.RefReshBtn.Name = "RefReshBtn";
+            this.RefReshBtn.Size = new System.Drawing.Size(119, 39);
+            this.RefReshBtn.TabIndex = 4;
+            this.RefReshBtn.Text = "Refresh";
+            this.RefReshBtn.UseVisualStyleBackColor = true;
+            this.RefReshBtn.Click += new System.EventHandler(this.RefReshBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 569);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private TextBox textBox1;
+        private Button EndBtn;
+        private Button StartBtn;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox1;
+        private Panel panel1;
+        private Button button4;
+        private Button button5;
+        private Button button3;
+        private Button RefReshBtn;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+
+namespace SystemProgramming_HW1;
+
+public partial class Form1 : Form
+{
+    Process process=new Process();
+    public Form1()
+    {
+        InitializeComponent();
+        FillProcess();
+    }
+
+    private void FillProcess()
+    {
+        foreach (var item in Process.GetProcesses())
+        {
+            listBox1.Items.Add(item.Id);
+            listBox2.Items.Add(item.ProcessName);
+            listBox3.Items.Add(item.BasePriority);
+        }
+    }
+
+    private void StartBtn_Click(object sender, EventArgs e)
+    {
+        Process.Start(textBox1.Text);
+    }
+
+    private void EndBtn_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string SelectedItem = listBox2.SelectedItem.ToString();
+            var SelectedProcess = Process.GetProcessesByName(SelectedItem).ToList();
+            foreach (var item in SelectedProcess)
+            {
+                item.Kill();
+                SelectedProcess.Remove(item);
+            }
+        }
+        catch (Exception ex)
+        {
+
+            MessageBox.Show(ex.Message);
+        }
+       
+    }
+
+    private void RefReshBtn_Click(object sender, EventArgs e)
+    {
+        listBox1.Items.Clear();
+        listBox2.Items.Clear();
+        listBox3.Items.Clear();
+        FillProcess();
+    }
+}
